01a3eab6b6c414d67f6da4b4a7b738173739f6ac
ArielEspinoza07 <arielespinoza46@gmail.com>
2016-03-04 14:08:17 -0500

    update package.json gruntfile.js and node_modules

--- gruntfile.js
+++ gruntfile.js
@@ -1,30 +1,72 @@
 module.exports = function(grunt){
   //Configuration of grunt.js
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json');
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['src/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    jshint:{
+      files: ['gruntfile.js','src/**/*','test/**/*.js'],
+      options: {
+        globals : {
+          jQuery: true,
+          console:true,
+          module:true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files%>'],
+      tasks: ['jshint','qunit']
+    },
     //uglify
     uglify: {
-      options :{
+      options: {
         mangle: false,
-        compress:{
+        compress: {
           drop_console: true
         }
-        //banner: '/*! <%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
-      },
-      js:{
-        files:[{
-          cwd: 'js/src/', //ruta de nuestro javascript fuente
-          expand: true, // ingresar a las subcarpetas
-          src: '*.js', // patron realtivo a cwd
-          dest: 'js/min/' //destino de los archivos compresos
-        }]
+        banner: '/*! <%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
+      },dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest%>']
+        }
       }
-
+    },
+    //javascript
+    js:{
+      files:[{
+        cwd: 'js/src/', //ruta de nuestro javascript fuente
+        expand: true, // ingresar a las subcarpetas
+        src: '*.js', // patron realtivo a cwd
+        dest: 'js/min/' //destino de los archivos compresos
+      }]
+    },
+    qunit: {
+      files: ['gruntfile.js','src/**/*.js','test/**/*.js']
     }
   });
 
   //Load the plugins that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   //Default task(s).
-  grunt.registerTask('default',['uglify']);
+  grunt.registerTask('test',['jshint','qunit']);
+
+  grunt.registerTask('default',['jshint','qunit','concat','uglify']);
+
+  grunt.registerTask('dist',['concat:dist','uglify:dist']);
 }
